f8aa800ec0a45dd845d15b428260974220f4c943
Aditya Kishor Dahal <toadityadahal@gmail.com>
2024-08-07 17:37:23 +0545

    add: People Serializer added, GET and POST method

--- home/views.py
+++ home/views.py
@@ -1,7 +1,22 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from home.models import Person
+from .serializers import PeopleSerializers
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def index(request):
-    return Response({"message": "Hello, world!"})
+    if request.method == 'GET':
+        responseObj = Person.objects.all()
+        personSerializer = PeopleSerializers(responseObj, many= True)
+        return Response(personSerializer.data)
+    else :
+        print(request.data)
+        data = request.data
+        personSerializer = PeopleSerializers(data = data)
+        print(personSerializer)
+        if personSerializer.is_valid():
+            personSerializer.save()
+            return Response(personSerializer.data)
+        else:
+            return Response(personSerializer.errors)
